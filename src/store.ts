5e3f5cff9bf0379892752e1152e8a70cf08b2211
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-07-04 11:07:40 +0900

    Refactor: redux => redux-toolkit

--- src/store.ts
+++ src/store.ts
@@ -1,54 +1,90 @@
+import {
+  configureStore,
+  createAction,
+  createReducer,
+  createSlice,
+} from "@reduxjs/toolkit";
 import { AnyAction, createStore, Reducer } from "redux";
 import { getLocalStorage } from "./utils/getLocalStorage";
 import { saveLocalStorage } from "./utils/saveLocalStorage";
-// import { getLocalStorage } from "./utils/getLocalStorage";
-// import { saveLocalStorage } from "./utils/saveLocalStorage";
-
-enum TO_DO {
-  ADD = "ADD",
-  DELETE = "DELETE",
-}
 
 export interface IToDo {
   text: string;
   id: number;
 }
+/**  vanilla redux  */
 
-export const addToDo = (text: string) => {
-  return {
-    type: TO_DO.ADD,
-    text,
-  };
-};
+// enum TO_DO {
+//   ADD = "ADD",
+//   DELETE = "DELETE",
+// }
 
-export const deleteToDo = (id: number) => {
-  return {
-    type: TO_DO.DELETE,
-    id,
-  };
-};
+// export const addToDo = (text: string) => {
+//   return {
+//     type: TO_DO.ADD,
+//     text,
+//   };
+// };
 
-const reducer: Reducer = (
-  state: IToDo[] = getLocalStorage("toDos") || [],
-  action: AnyAction
-) => {
-  switch (action.type) {
-    case TO_DO.ADD:
-      return saveLocalStorage([
-        { text: action.text, id: Date.now() },
-        ...state,
-      ]);
-    case TO_DO.DELETE:
-      return saveLocalStorage(
-        state.filter((toDo) => {
-          return toDo.id !== parseInt(action.id);
-        })
-      );
-    default:
-      return state;
-  }
-};
+// export const deleteToDo = (id: number) => {
+//   return {
+//     type: TO_DO.DELETE,
+//     id,
+//   };
+// };
 
-const store = createStore(reducer);
+// const reducer: Reducer = (
+//   state: IToDo[] = getLocalStorage("toDos") || [],
+//   action: AnyAction
+// ) => {
+//   switch (action.type) {
+//     case TO_DO.ADD:
+//       return saveLocalStorage([
+//         { text: action.text, id: Date.now() },
+//         ...state,
+//       ]);
+//     case TO_DO.DELETE:
+//       return saveLocalStorage(
+//         state.filter((toDo) => {
+//           return toDo.id !== parseInt(action.id);
+//         })
+//       );
+//     default:
+//       return state;
+//   }
+// };
+// const store = createStore(reducer);
+
+/** redux-toolkit  */
+
+// export const addToDo = createAction<string>("ADD");
+// export const deleteToDo = createAction<number>("DELETE");
+
+const initialState = (getLocalStorage() || []) as IToDo[];
+
+// const reducer = createReducer(initialState, (builder) => {
+//   builder
+//     .addCase(addToDo, (state, action) =>
+//       saveLocalStorage([{ text: action.payload, id: Date.now() }, ...state])
+//     )
+//     .addCase(deleteToDo, (state, action) =>
+//       saveLocalStorage(state.filter((toDo) => toDo.id !== action.payload))
+//     );
+// });
+
+const toDos = createSlice({
+  name: "toDosSlice",
+  initialState,
+  reducers: {
+    add: (state, action) =>
+      saveLocalStorage([{ text: action.payload, id: Date.now() }, ...state]),
+    remove: (state, action) =>
+      saveLocalStorage(state.filter((toDo) => toDo.id !== action.payload)),
+  },
+});
+
+export const { add, remove } = toDos.actions;
+
+const store = configureStore({ reducer: toDos.reducer });
 
 export default store;
